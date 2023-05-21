cab7723c55d0c3a663af9a6cf85466e2c1ffd60e
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-22 02:26:59 +0600

    firebase env set

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAH7Ru4ZD7JGrVr6l8DUCL6vba8TY7zR50",
-  authDomain: "lego-mart.firebaseapp.com",
-  projectId: "lego-mart",
-  storageBucket: "lego-mart.appspot.com",
-  messagingSenderId: "879204489260",
-  appId: "1:879204489260:web:54e6437dbe78f6d217d989"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
